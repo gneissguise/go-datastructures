3ce5467c78cacb472c397439fc37ef19999e3f73
Justin Frost <justinfrost@duck.com>
2025-04-28 20:10:42 -0700

    feat: linkedlist implementation

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -17,3 +17,26 @@ type LinkedList[T any] struct {
 func New[T any]() *LinkedList[T] {
 	return &LinkedList[T]{}
 }
+
+// Length returns number of items in the list
+func (l *LinkedList[T]) Length() int {
+	return l.length
+}
+
+// Head returns the first item in the list
+func (l *LinkedList[T]) Head() (T, bool) {
+	if l.head == nil {
+		var zero T
+		return zero, false
+	}
+	return l.head.Value, true
+}
+
+// Tail returns the last item in the list
+func (l *LinkedList[T]) Tail() (T, bool) {
+	if l.tail == nil {
+		var zero T
+		return zero, false
+	}
+	return l.tail.Value, true
+}
